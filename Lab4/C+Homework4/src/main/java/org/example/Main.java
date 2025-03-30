6eb417281cd102b808d783178462a301ffe7fd09
bostangeorgiana <161864553+bostangeorgiana@users.noreply.github.com>
2025-03-30 20:52:55 +0300

    Update Main.java

--- Lab4/C+Homework4/src/main/java/org/example/Main.java
+++ Lab4/C+Homework4/src/main/java/org/example/Main.java
@@ -7,21 +7,18 @@ import org.jgrapht.graph.DefaultEdge;
 
 public class Main {
     public static void main(String[] args) {
-        // creem locatii
         Location loc1 = new Location("City A", LocationType.FRIENDLY);
         Location loc2 = new Location("City B", LocationType.ENEMY);
         Location loc3 = new Location("City C", LocationType.NEUTRAL);
         Location loc4 = new Location("City D", LocationType.FRIENDLY);
 
-        // Adăugăm locațiile într-o listă
+       
         List<Location> locations = Arrays.asList(loc1, loc2, loc3, loc4);
 
-        // Mapa cu timpi între locații
         Map<Location, Map<Location, Integer>> timeMap = new HashMap<>();
 
-        // Exemplu de timpi între locații (poți să adaugi mai mulți timpi aici)
         Map<Location, Integer> fromLoc1 = new HashMap<>();
-        fromLoc1.put(loc2, 5); // Timpul de la loc1 la loc2 este 5 minute
+        fromLoc1.put(loc2, 5); 
         fromLoc1.put(loc3, 10);
         timeMap.put(loc1, fromLoc1);
 
@@ -37,10 +34,8 @@ public class Main {
         fromLoc4.put(loc1, 3);
         timeMap.put(loc4, fromLoc4);
 
-        // Creăm procesorul de locații
         LocationProcessor processor = new LocationProcessor(locations, timeMap);
 
-        // Afișăm timpii
-        processor.printShortestTimes(loc1);  // Afișează timpii începând de la loc1
+        processor.printShortestTimes(loc1); 
     }
 }
